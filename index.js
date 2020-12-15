1938394b3f90b5a0fe2102ae74471d9d5b8d8d5c
Adam Priest <adampriest@pop-os.localdomain>
2020-12-14 19:07:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+// selects "dodger" element and saves to a variable
+let dodger = document.getElementById("dodger");
+
+// adds feature to move "dodger" 'left' and 'right'
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerLeft();
+    }
+});
+
+// moveDodgerLeft & moveDodgerRight functions
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight () {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
